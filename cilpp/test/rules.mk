b83c4287cd621fa17c9b53a6397082d12c0dfb92
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-07-29 16:37:34 +0100

    Update wrapper logic to avoid quoting issues when setting multi-word CPP command. Also add a hello-world test for cilpp.

---
+++ cilpp/test/rules.mk
@@ -0,0 +1,7 @@
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+CILPP_PREFIX ?= $(realpath $(dir $(THIS_MAKEFILE))/..)
+
+BASIC_CFLAGS += -save-temps
+
+# We build all our tests by running them through our cccppp wrapper.
+CFLAGS += `$(CILPP_PREFIX)/bin/cilpp-cflags` $(BASIC_CFLAGS)
