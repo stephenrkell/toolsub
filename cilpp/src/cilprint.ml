890d0bd49bba2b09e8caa04e240e7f678b979837
Stephen Kell <srk31@srcf.ucam.org>
2025-02-18 21:46:00 +0000

    cilpp: fix bug that was not fully processing args received via CC_IDENTIFIED_ARGS mechanism, and do some cleanup

--- cilpp/src/cilprint.ml
+++ cilpp/src/cilprint.ml
@@ -1,16 +1,4 @@
 open Unix
 open Cilpp_common
 
-let () =
-    let (currentCilFile, outputFile) = prepareCilFile Sys.argv in
-    Cil.printerForMaincil := Cil.defaultCilPrinter;
-    (* We are not printing for CIL input *)
-    Cil.print_CIL_Input := false;
-    let (chan, str) = match !outputFile with
-            None -> Pervasives.stdout, "(stdout)"
-          | Some(fname) -> (Pervasives.open_out fname, fname)
-    in
-    let _ = Cil.dumpFile Cil.plainCilPrinter chan str currentCilFile
-    in
-    let status = if !Errormsg.hadErrors then 1 else 0 in
-    exit status
+let () = runWithPrinter Cil.plainCilPrinter
