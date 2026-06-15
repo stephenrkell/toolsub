5002ef8207f291cab3cb3aa708ae6bb2ee09893f
Stephen Kell <srk31@srcf.ucam.org>
2026-06-15 20:11:03 +0100

    cilpp: improve comments to src/goblintCil_compat.ml

--- cilpp/src/goblintCil_compat.ml
+++ cilpp/src/goblintCil_compat.ml
@@ -1,10 +1,6 @@
 module Cil = GoblintCil
-(* open Unix *)
-(*open GoblintCil*)
-(* open Cil *)
-(* open MainFeature *)
-(* open Feature *)
-module Feature = Cil.Feature
+(* XXX: for clients that don't 'open' Cil, why are the below correct? *)
+module Feature = Cil.Feature (* XXX: what about stuff moved into MainFeature by goblint? *)
 module Cprint = Cil.Cprint
 module Frontc = Cil.Frontc
 module Errormsg = Cil.Errormsg
